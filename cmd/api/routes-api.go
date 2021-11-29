a6ac560c5f423506477718821dae9482297bf008
Peter Nunn <pnunn@infoteq.com.au>
2021-11-29 16:53:37 +1100

    End of CH 85

--- cmd/api/routes-api.go
+++ cmd/api/routes-api.go
@@ -14,15 +14,15 @@ func (app *application) routes() http.Handler {
 		AllowedOrigins:   []string{"http://*", "https://*"},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"Accept", "Accept-Language", "Authorization", "Content-Type", "X-CSRF-Token"},
-		AllowCredentials: true,
+		AllowCredentials: false,
 		MaxAge:           300,
-		Debug:            true,
 	}))
 
 	mux.Post("/api/payment-intent", app.GetPaymentIntent)
 	mux.Get("/api/widget/{id}", app.GetWidgetByID)
 	mux.Post("/api/create-customer-and-subscribe-to-plan", app.CreateCustomerAndSubscribeToPlan)
 	mux.Post("/api/authenticate", app.CreateAuthToken)
+	mux.Post("/api/is-authenticated", app.CheckAuthentication)
 
 	return mux
 }
