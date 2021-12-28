1dc1a587e05f13744dbb6b710ebd7580ca7719fc
Peter Nunn <pnunn@infoteq.com.au>
2021-12-28 20:38:22 +1100

    End of CH 131

--- internal/models/models.go
+++ internal/models/models.go
@@ -497,3 +497,16 @@ where
 
 	return o, nil
 }
+
+func (m *DBModel) UpdateOrderStatus(id, statusID int) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	stmt := "update orders set status_id = ? where id = ?"
+
+	_, err := m.DB.ExecContext(ctx, stmt, statusID, id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
